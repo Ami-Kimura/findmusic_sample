0bceddde49ae2fd8df196622711adf4040b1ad34
Ami-Kimura <82437518+Ami-Kimura@users.noreply.github.com>
2023-07-28 13:50:40 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
+const app = express(); 
+
+
+app.use(cors());
+
+
+app.use(express.static('web'));
+
+
+app.get('/list', (req, res) => { 
+  
+  var client_id = "";
+  var client_secret = ""; 
+
+ 
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'client_credentials'
+    },
+    json: true 
+  };
+
+ 
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) { 
+
+     
+      var token = body.access_token;
+      var options = {
+        url: 'https://api.spotify.com/v1/playlists/6VSaqaoH2s3x0xMJCUeDzV/tracks', 
+        headers: { 'Authorization': 'Bearer ' + token,
+         },
+        json: true
+      };
+
+      request.get(options, function (error, response, body) {
+        res.json(body);
+      });
+    }
+  });
+});
+
+console.log('Listening on 3000');
+app.listen(3000);
